704cb058977a1236483acd55953441f9e709bac1
deeppatel007 <deepp0254@gmail.com>
2021-06-15 18:55:33 +0530

    1

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
 const app = express();
 const PORT = 8000;
+const URL = 'mongodb+srv://deeppatel:<deeppatel1204>@flipkart.ud4lx.mongodb.net/FLIPKART?retryWrites=true&w=majority';
+const Connection = (URL,(useNewUrlParser: true, useUnifiedTopolog:true,usefindAndModify:false))=>{
+
+}
 app.listen(PORT,()=>console.log(`server is successfully running on ${PORT} `));
